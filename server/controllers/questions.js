df4913948dc170b4f3449955bf0b18030f7480ff
Gary Ong <gary@x0pa.com>
2021-01-22 11:45:37 +0800

    added get questionnaire

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -5,8 +5,19 @@ const getQuestions = async (request,h)=>{
         let x =  await Questionnaire.findAll({
             include:[{
                 model:Questiontype,
-                as:"QuestionType"
-            }]
+                as:"QuestionType",
+                attributes:["question_type_name"],
+                required:true
+            },{
+                model:Company,
+                as:"Company",
+                attributes:[],
+                where:{
+                    companyName:COMPANY_NAME
+                }
+            }],
+            attributes:["question_id","question_name","question_config"],
+            required:true
         })
         return x
     }catch(err){
