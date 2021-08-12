db934c4017ed879d628d4bdb000c02e690f262d7
ManashX0PA <Manash@x0pa.com>
2021-08-12 09:17:50 +0530

    added isCompanyOnboardingComplete on login API

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+import { camelizeKeys } from '../utils/camelizeKeys';
 
 const loginUser = async (request, h) => {
   try {
@@ -12,73 +13,80 @@ const loginUser = async (request, h) => {
     const { User, Userinfo, Accesstoken, Usertype, Userrole } = request.getModels('xpaxr');
     const { email: rEmail, password } = request.payload || {};
     const email = rEmail?.toLowerCase();
-    
-    if ( !(email && password) ) {
-      return h.response({ error: true, message: 'Please provide necessary credentials'}).code(400);
+
+    if (!(email && password)) {
+      return h.response({ error: true, message: 'Please provide necessary credentials' }).code(400);
     }
 
     if (!validator.isEmail(email)) {
-      return h.response({ error: true, message: 'Please provide a valid Email'}).code(400);
+      return h.response({ error: true, message: 'Please provide a valid Email' }).code(400);
     }
     if (password.length < 8 || password.length > 100) {
-      return h.response({ error: true, message: 'Invalid Password'}).code(400);
+      return h.response({ error: true, message: 'Invalid Password' }).code(400);
     }
 
     const udata = await User.findOne({
-        where: {
-          email
-        },
-        attributes: ['email', 'password', 'user_id']
+      where: {
+        email
+      },
+      attributes: ['email', 'password', 'user_id']
     });
     const user = udata && udata.toJSON();
     if (!user) {
-      return h.response({ error: true, message: 'Wrong Email or Password!'}).code(400);
+      return h.response({ error: true, message: 'Wrong Email or Password!' }).code(400);
     }
-    const {password: passwordStored, user_id} = udata && udata.toJSON();
+    const { password: passwordStored, user_id } = udata && udata.toJSON();
     const isPasswordMatching = bcrypt.compareSync(password, passwordStored);
     if (!isPasswordMatching) {
-      return h.response({ error: true, message: 'Wrong Email or Password!'}).code(400);
+      return h.response({ error: true, message: 'Wrong Email or Password!' }).code(400);
     }
 
-    const uidata = await Userinfo.findOne({
-        where:{
-            email
-        },
-        attributes: {
-            exclude: ['createdAt', 'updatedAt']
-        }
+    // get user record info
+    const db1 = request.getDb('xpaxr');
+    const sqlStmt = `select 
+       c.is_company_onboarding_complete,
+       ur.role_name, ut.user_type_name, ui.*
+     from hris.userinfo ui
+       inner join hris.userrole ur on ur.role_id=ui.role_id
+       inner join hris.usertype ut on ut.user_type_id=ui.user_type_id
+       left join hris.company c on c.company_id=ui.company_id
+     where ui.email=:email`;
+
+    const sequelize = db1.sequelize;
+    const userRecordSQL = await sequelize.query(sqlStmt, {
+      type: QueryTypes.SELECT,
+      replacements: { email }
     });
-    const userInfoRes = uidata && uidata.toJSON();
-    const { 
-        userUuid, 
-        userTypeId, 
-        email: userEmail, 
-        roleId, 
-        active,
-        companyId,
-        companyUuid,
-        firstName,
-        lastName,
-        isAdmin,
-        tzid,
-        primaryMobile
+    const userInfoRes = camelizeKeys(userRecordSQL)[0];
+
+    const {
+      userUuid,
+      userTypeId,
+      email: userEmail,
+      roleId,
+      active,
+      companyId,
+      companyUuid,
+      firstName,
+      lastName,
+      isAdmin,
+      tzid,
+      primaryMobile,
+
+      userTypeName,
+      roleName,
     } = userInfoRes || {};
-    let [userTypeRecord,userRoleRecord] = await Promise.all([Usertype.findOne({ where: { userTypeId }}),Userrole.findOne({ where: { roleId }})])
-    const { userTypeName } = userTypeRecord
-    const { roleName } = userRoleRecord
-    const token = await jwt.sign({userUuid, userTypeId, email: userEmail, roleId, active, companyId, companyUuid, firstName, lastName, isAdmin, tzid, primaryMobile,userTypeName,userRoleRecord}, config.get('jwtSecret'), {expiresIn: '24h'});
+    const token = await jwt.sign({ userUuid, userTypeId, email: userEmail, roleId, active, companyId, companyUuid, firstName, lastName, isAdmin, tzid, primaryMobile, userTypeName, roleName }, config.get('jwtSecret'), { expiresIn: '24h' });
     await Accesstoken.create({
       token,
       userId: user_id,
       isValid: true
     });
-    userInfoRes.userTypeName = userTypeName;
-    userInfoRes.roleName = roleName;
-    return h.response({user: userInfoRes, token}).code(200);
-  } 
+    return h.response({ user: userInfoRes, token }).code(200);
+  }
   catch (error) {
     console.error(error.stack);
-    return h.response({error: true, message: 'Bad Request'}).code(500);
+    return h.response({ error: true, message: 'Bad Request' }).code(500);
   }
 };
 
