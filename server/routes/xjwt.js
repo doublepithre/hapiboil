db81748a769b32df26cdbc8880cf4a98c3e690bb
ManashX0PA <Manash@x0pa.com>
2021-08-30 10:59:57 +0530

    expiration time of login token added (12 hrs)

--- server/routes/xjwt.js
+++ server/routes/xjwt.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { QueryTypes } from 'sequelize';
 import { formatQueryRes } from '../utils/index';
 const jwt = require('jsonwebtoken');
@@ -11,16 +12,22 @@ const xjwt = {
         server.auth.strategy('jwt', 'jwt', {
             key: config.get('jwtSecret'),
             validate: async (decoded, request, h) => {
-                const {authorization} = request.headers || {};
+                const { authorization } = request.headers || {};
                 const token = authorization.split(' ')[1];
                 const db1 = request.getDb('xpaxr');
-                const sqlStmt = `select * from hris.accesstoken ato
-                        inner join hris.userinfo ui on ato.user_id=ui.user_id where ato.token= :token and is_valid=true`;
+                const sqlStmt = `select *
+                    from hris.accesstoken ato
+                            inner join hris.userinfo ui on ato.user_id=ui.user_id
+                        where
+                            ato.token= :token and is_valid=true
+                            and ato.created_at > :expirationTime`;
                 const sequelize = db1.sequelize;
+                const expirationTime = moment().subtract('12', 'hours').toISOString();
+                console.log({ expirationTime });
                 const ares = await sequelize.query(sqlStmt, {
-                        type: QueryTypes.SELECT,
-                        replacements: { token: token },
-                    });
+                    type: QueryTypes.SELECT,
+                    replacements: { token: token, expirationTime },
+                });
                 const uinfo = formatQueryRes(ares);
                 const { userId } = uinfo || {};
 
@@ -30,27 +37,27 @@ const xjwt = {
                     inner join hris.usertype ut on ut.user_type_id=ui.user_type_id
                 where user_id=:userId`;
                 const ares2 = await sequelize.query(sqlStmt2, {
-                        type: QueryTypes.SELECT,
-                        replacements: { userId },
-                    });
+                    type: QueryTypes.SELECT,
+                    replacements: { userId },
+                });
                 const uinfoData = formatQueryRes(ares2);
                 const { userTypeName } = uinfoData || {};
-                
+
                 if (userTypeName === 'leadership' || userTypeName === 'supportstaff') {
                     return { isValid: false, credentials: {}, artifacts: {} };
                 }
 
-                if(userId) {
+                if (userId) {
                     const credentials = { id: userId, token };
                     const artifacts = { luser: uinfo, };    // as per docs, artifacts saves only token & decoded info. So luser info won't be saved.
                     const { active } = uinfo || {};
-                    let isValid = true;                    
-                    if(!active) {
-                        isValid= false;
+                    let isValid = true;
+                    if (!active) {
+                        isValid = false;
                         console.log(isValid);
                         console.log(uinfo);
-                        return h.response({ error: true, message: 'User account is inactive!'}).code(400);
-                    }                    
+                        return h.response({ error: true, message: 'User account is inactive!' }).code(400);
+                    }
                     return { isValid: isValid, credentials, artifacts };
                 } else {
                     return { isValid: false, credentials: {}, artifacts: {} };
