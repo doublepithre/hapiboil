e63fb2e8162d7754cd6dfcc4a4b11284e1a2fd4a
ManashX0PA <Manash@x0pa.com>
2021-09-01 15:33:18 +0530

    added leadershipMessage on the login response

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -45,6 +45,7 @@ const loginUser = async (request, h) => {
     const db1 = request.getDb('xpaxr');
     const sqlStmt = `select 
        c.is_company_onboarding_complete,
+       c.leadership_message,
        ur.role_name, ut.user_type_name, ui.*
      from hris.userinfo ui
        inner join hris.userrole ur on ur.role_id=ui.role_id
@@ -90,6 +91,9 @@ const loginUser = async (request, h) => {
       const { umetaId, metaValue } = userMetaData || {};
 
       userInfoRes.isOnboardingComplete = umetaId ? metaValue : "no";
+    }
+    if (userTypeName !== 'supervisor' && userTypeName !== 'workbuddy') {
+      delete userInfoRes.leadershipMessage;
     }
     if (userTypeName !== 'companysuperadmin') {
       delete userInfoRes.isCompanyOnboardingComplete;
