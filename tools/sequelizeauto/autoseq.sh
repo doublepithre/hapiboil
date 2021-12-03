57868d211bbc8d69984b1aefc7dcf3d722c3e4f2
ManashX0PA <Manash@x0pa.com>
2021-12-03 12:27:23 +0530

    BOILER_PLATE

--- tools/sequelizeauto/autoseq.sh
+++ tools/sequelizeauto/autoseq.sh
@@ -1,7 +1,7 @@
 rm ../../server/models/definition/*
 rm models/*
 rm convertedModels/*
-../../node_modules/.bin/sequelize-auto sequelize-auto -d canopus -h localhost -p 5432 -u postgres -x \$ilven1eaf -e postgres -s hris --cm p --cp c --sg
+../../node_modules/.bin/sequelize-auto sequelize-auto -d DATABASE_NAME -h localhost -p 5432 -u DATABASE_USER -x DATABASE_PASSWORD -e postgres -s hris --cm p --cp c --sg
 npx github:gary-x0pa/sequelize-auto2hapi-sequelizejs
 node rename.js
 cp -R convertedModels/* ../../server/models/definition/
